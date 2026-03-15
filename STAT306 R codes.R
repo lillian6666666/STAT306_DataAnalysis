ff35cc7e51f9c5a967714352c13bb83fd9de5e15
Lillian Chen <rchen54@students.cs.ubc.ca>
2026-03-15 09:21:51 -0700

    updated

--- STAT306 R codes.R
+++ STAT306 R codes.R
@@ -4,3 +4,7 @@ Life.Expectancy.Data.2 <- read.csv("~/Downloads/Life Expectancy Data 2.csv",
                                    header=FALSE)
 Data <- Life.Expectancy.Data.2
 
+## Directly read 
+library(tidyverse)
+data <- read_csv("https://drive.google.com/uc?export=download&id=1A_Nkqsxh4ymFIJDj7Fbo1SOCoVujhbjb")
+data
