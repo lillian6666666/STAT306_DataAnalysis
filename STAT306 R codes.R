a3c81037136be5b2b642770cfa1ccc09ab038bfa
Ruiting Jiang <jiangruiting1116@163.com>
2026-03-15 09:59:29 -0700

    qqplot and residual plot

--- STAT306 R codes.R
+++ STAT306 R codes.R
@@ -12,4 +12,30 @@ data
 mean(data$`Life expectancy`, na.rm = TRUE)
 median(data$`Life expectancy`, na.rm = TRUE)
 sd(data$`Life expectancy`, na.rm = TRUE)
-nrow(data$`Life expectancy`, na.rm = TRUE)
+nrow(data$`Life expectancy`)
+
+# rename variable for easier use
+data <- data %>%
+  rename(
+    LifeExpectancy = "Life expectancy",
+  )
+
+data<- data|> select(-Country)|> filter(Year==2015)
+data|> ggplot(aes(x=Schooling, y= LifeExpectancy, color=Status))+geom_point()
+# reg <-lm(Life_Expectancy ~ Schooling *Status, data)
+# summary(reg)
+# qqplot(reg)
+# resid.plot(reg)
+data$Status <- as.factor(data$Status)
+
+# Fit regression
+model <- lm(LifeExpectancy ~ Schooling *Status, data = data)
+summary(model)
+
+# Residuals vs Fitted
+plot(model$fitted.values, model$residuals,
+     xlab="Fitted value", ylab="Residual")
+
+# Q-Q plot
+qqnorm(resid(model))
+qqline(resid(model))
